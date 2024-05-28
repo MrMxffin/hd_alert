a7b5d5e8b2dfa91a2e3d0adabbc4c16a242bde70
Robert <rs75lovo@studserv.uni-leipzig.de>
2024-05-28 18:54:43 +0200

    add subscribing functionality

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,60 +4,103 @@ from dotenv import load_dotenv
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes, CallbackQueryHandler
 import requests
-import time
 from datetime import datetime, timedelta
 
+# Load environment variables from .env file
+load_dotenv()
+
 # Global dictionary to store vote counts for each location message
-DATA_FILE = ".data/tracked_messages.json"
+data_path = os.getenv('PATH_TO_TRACKED_MESSAGES')
 tracked_messages = {}
 
 
+def initialize_file(path, default_data):
+    """Ensure the file exists and contains the default data if not."""
+    if not os.path.exists(path):
+        with open(path, 'w') as file:
+            json.dump(default_data, file, indent=2)
+
 def load_tracked_messages():
     global tracked_messages
-    if os.path.exists(DATA_FILE):
-        with open(DATA_FILE, 'r') as file:
+    initialize_file(data_path, {"messages": []})  # Ensure the file exists
+    try:
+        with open(data_path, 'r') as file:
             tracked_messages = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error loading tracked messages: {e}")
+        tracked_messages = {"messages": []}  # Reset to default if there's an error
+
 
 
 def save_tracked_messages():
-    with open(DATA_FILE, 'w') as file:
-        json.dump(tracked_messages, file, indent=4)
+    with open(data_path, 'w') as file:
+        json.dump(tracked_messages, file, indent=2)
 
 
 def clean_old_messages():
     current_time = datetime.now()
-    messages_to_delete = [key for key, value in tracked_messages.items() if
-                          datetime.fromisoformat(value["delete_time"]) < current_time]
-    for key in messages_to_delete:
-        del tracked_messages[key]
+    messages_to_keep = [msg for msg in tracked_messages["messages"] if
+                        datetime.fromisoformat(msg["delete_time"]) > current_time]
+    tracked_messages["messages"] = messages_to_keep
     save_tracked_messages()
 
 
 # Load tracked messages at the start
 load_tracked_messages()
 
-# Load environment variables from .env file
-load_dotenv()
-
 # Get the token from environment variables
 TOKEN = os.getenv('TELEGRAM_TOKEN')
+OWNER_ID = os.getenv('OWNER_ID')
 
 
-# Define a function to handle the /start command
+# Define the function to handle the /start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     reply_keyboard = [[
         {"text": "Sende derzeitigen Standort.", "request_location": True},
     ]]
     # Ask the user for their location
-    await update.message.reply_text(
+    await update.effective_message.reply_text(
         "Bitte sende den Ort der Hausdurchsuchung.",
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
     )
 
 
-# Define a function to handle the location message
+async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Check if the message is from a private chat, group, or channel
+    query = ""
+    if update.effective_message.message_thread_id:
+        message_thread_id = update.effective_message.message_thread_id
+    else:
+        message_thread_id = None
+    user = update.effective_user
+    chat = update.effective_chat
+    if update.effective_chat.type == "supergroup":
+        query = f"The User @{user.username} (ID: {user.id}) is requesting to subscribe the supergroup {chat.title} (ID: {chat.id}). Do you approve?"
+    elif update.effective_chat.type == "group":
+        query = f"The User @{user.username} (ID: {user.id}) is requesting to subscribe the group {chat.title} (ID: {chat.id}). Do you approve?",
+    elif update.effective_chat.type == "channel":
+        query = f"The Channel {chat.title} (ID: {chat.id}) is requesting to subscribe). Do you approve?",
+    elif update.effective_chat.type == "private":
+        query = f"User @{user.username} (ID: {user.id}) is requesting to subscribe to the Bot in a private chat. Do you approve?",
+
+    if is_chat_subscribed(chat.id, message_thread_id):
+        await update.effective_message.reply_text("You are already subscribed.")
+    else:
+        await context.bot.send_message(
+            OWNER_ID,
+            query,
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("Yes", callback_data=f"approve_{chat.id}_{message_thread_id}"),
+                 InlineKeyboardButton("No", callback_data=f"reject_{chat.id}_{message_thread_id}")]
+            ])
+        )
+        await update.effective_message.reply_text("Your subscription request has been sent to the bot owner.")
+
+
+# Define the function to handle the location message
 def get_channels(path):
     try:
+        initialize_file(path, {"channels": []})  # Ensure the file exists
         with open(path, 'r') as file:
             channels_data = json.load(file)
             return channels_data.get('channels', [])
@@ -67,6 +110,14 @@ def get_channels(path):
     except json.JSONDecodeError:
         print(f"Error: Invalid JSON format in file: {path}")
         return []
+
+
+def is_chat_subscribed(chat_id, message_thread_id):
+    channels = get_channels(os.getenv("PATH_TO_CHANNELS"))
+    filtered_data = [entry for entry in channels if entry["chat_id"] == chat_id]
+    if message_thread_id is not None:
+        filtered_data = [entry for entry in filtered_data if "message_thread_id" in entry and entry["message_thread_id"] == message_thread_id]
+    return len(filtered_data)
 
 
 def get_location_name(location):
@@ -103,20 +154,31 @@ def get_location_name(location):
 # Define a function to handle the location message
 async def handle_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # Check if a location was shared
-    if update.message.location:
-        location = update.message.location
+    if update.effective_message.location:
+        location = update.effective_message.location
         username = update.effective_user.username
         # Initialize vote counts for the new message
         address = get_location_name(location)
         delete_time = (datetime.now() + timedelta(weeks=1)).isoformat()
-        tracked_messages[f"{update.effective_user.id}_{update.message.message_id}"] = {
-            "valid": 0,
-            "invalid": 0,
-            "address": address,
-            "username": username,
-            "user_votes": [],
-            "delete_time": delete_time
-        }
+
+        existing_message = next((msg for msg in tracked_messages["messages"] if msg["address"] == address), None)
+        if existing_message:
+            existing_message["messages"].append({
+                "chat_id": update.effective_chat.id,
+                "message_id": update.effective_message.message_id
+            })
+        else:
+            tracked_messages["messages"].append({
+                "address": address,
+                "username": username,
+                "user_votes": {"valid": [], "invalid": []},
+                "delete_time": delete_time,
+                "messages": [{
+                    "chat_id": update.effective_chat.id,
+                    "message_id": update.effective_message.message_id
+                }]
+            })
+
         # Save the updated tracked messages
         save_tracked_messages()
         # Use your existing logic to send the location to the channels
@@ -126,27 +188,28 @@ async def handle_location(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
             message_thread_id = channel.get('message_thread_id')
 
             # Send the location message with inline keyboard for voting
-            message = await context.bot.send_message(
-                chat_id,
-                f"Der Nutzer @{username} meldet eine Hausdurchsuchung an folgender Adresse:\n"
+            await context.bot.send_message(
+                chat_id=chat_id,
+                message_thread_id=message_thread_id,
+                text=f"Der Nutzer @{username} meldet eine Hausdurchsuchung an folgender Adresse:\n"
                 f"{address}\n",
                 reply_markup=InlineKeyboardMarkup([
                     [InlineKeyboardButton("Valid",
-                                          callback_data=f"valid_{update.effective_user.id}_{update.message.message_id}"),
+                                          callback_data=f"valid_{update.effective_user.id}_{update.effective_message.message_id}"),
                      InlineKeyboardButton("Invalid",
-                                          callback_data=f"invalid_{update.effective_user.id}_{update.message.message_id}")]
+                                          callback_data=f"invalid_{update.effective_user.id}_{update.effective_message.message_id}")]
                 ])
             )
 
             # Send the location coordinates
             await context.bot.send_location(
-                chat_id,
-                message_thread_id,
+                chat_id=chat_id,
                 location=location,
+                message_thread_id=message_thread_id
             )
 
         # Remove the keyboard
-        await update.message.reply_text("Vielen Dank für deine Hilfe.", reply_markup=ReplyKeyboardRemove())
+        await update.effective_message.reply_text("Vielen Dank für deine Hilfe.", reply_markup=ReplyKeyboardRemove())
 
 
 # Callback function to handle button clicks
@@ -156,50 +219,61 @@ async def button_click(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     user_id = update.effective_user.id
     original_user_id, message_id = id_parts[1], id_parts[2]
 
-    message = tracked_messages.get(f"{original_user_id}_{message_id}")
-    user_votes = message.get("user_votes", [])
-    for vote in user_votes:
-        if user_id in vote:
-            current_vote = vote[user_id]
-            if (query.data.startswith("valid") and current_vote == "valid") or \
-                    (query.data.startswith("invalid") and current_vote == "invalid"):
-                # No need to update counts or display an error message
-                return
-            # Update vote count based on the button clicked
-            if query.data.startswith("valid") and current_vote == "invalid":
-                message["valid"] += 1
-                message["invalid"] -= 1
-                vote[user_id] = "valid"
-            elif query.data.startswith("invalid") and current_vote == "valid":
-                message["valid"] -= 1
-                message["invalid"] += 1
-                vote[user_id] = "invalid"
-            break
-    else:
-        new_vote = "valid" if query.data.startswith("valid") else "invalid"
-        message["user_votes"].append({user_id: new_vote})
-        if new_vote == "valid":
-            message["valid"] += 1
+    message = next((msg for msg in tracked_messages["messages"] if any(
+        msg_id["chat_id"] == original_user_id and msg_id["message_id"] == int(message_id) for msg_id in
+        msg["messages"])), None)
+    if not message:
+        return
+
+    user_votes = message.setdefault("user_votes", {"valid": [], "invalid": []})
+    valid_votes = user_votes["valid"]
+    invalid_votes = user_votes["invalid"]
+
+    def switch_vote(vote_from, vote_to):
+        if user_id in vote_from:
+            vote_from.remove(user_id)
+            vote_to.append(user_id)
         else:
-            message["invalid"] += 1
+            vote_to.append(user_id)
+
+    if query.data.startswith("valid"):
+        if user_id not in valid_votes:
+            switch_vote(invalid_votes, valid_votes)
+    elif query.data.startswith("invalid"):
+        if user_id not in invalid_votes:
+            switch_vote(valid_votes, invalid_votes)
 
     # Save the updated tracked messages
     save_tracked_messages()
+
+    # Update message text to reflect new vote counts
     address = message["address"]
     username = message["username"]
-
-    # Attribution for OpenStreetMap(http://www.openstreetmap.org/copyright)
-    osmAttribution = "[Map data © OpenStreetMap contributors](http://www.openstreetmap.org/copyright)"
-
-    # Get the original message text shared by the user
-    original_text = f"Der Nutzer @{username} meldet eine Hausdurchsuchung an folgender Adresse:\n{address}\n{osmAttribution}"
-    votes = message['valid'] + message['invalid']
-    percent_valid = round(100 * message['valid'] / votes, 2)
-    # Edit the original message to show updated vote counts along with the original text
+    original_text = f"Der Nutzer @{username} meldet eine Hausdurchsuchung an folgender Adresse:\n{address}\n"
+    n_votes = len(valid_votes) + len(invalid_votes)
+    percent_valid = round(100 * len(valid_votes) / n_votes, 2)
     await query.message.edit_text(
-        text=f"{original_text}\n\nValidity: {percent_valid}%\nVotes: {votes}",
-        reply_markup=query.message.reply_markup  # Retain the existing reply markup
+        text=f"{original_text}\n\nValidity: {percent_valid}%\nVotes: {n_votes}",
+        reply_markup=query.message.reply_markup
     )
+
+
+# Callback function to handle subscription approval/rejection
+async def handle_subscription(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    id_parts = query.data.split('_')
+    action, chat_id, message_thread_id = id_parts[0], int(id_parts[1]), id_parts[2]
+    if action == "approve":
+        channels = get_channels(os.getenv("PATH_TO_CHANNELS"))
+        if message_thread_id == "None":
+            channels.append({"chat_id": chat_id})
+        else:
+            channels.append({"chat_id": chat_id, "message_thread_id": int(message_thread_id)})
+        with open(os.getenv("PATH_TO_CHANNELS"), 'w') as file:
+            json.dump({"channels": channels}, file, indent=2)
+        await query.message.edit_text("Subscription approved.")
+    elif action == "reject":
+        await query.message.edit_text("Subscription rejected.")
 
 
 def run_bot():
@@ -207,8 +281,11 @@ def run_bot():
         # Create an Updater object with your bot token
         application = ApplicationBuilder().token(TOKEN).build()
         application.add_handler(CommandHandler('start', start))
+        application.add_handler(
+            MessageHandler(filters=filters.COMMAND & filters.Regex(r'^/subscribe'), callback=subscribe))
         application.add_handler(MessageHandler(filters.LOCATION, handle_location))
-        application.add_handler(CallbackQueryHandler(button_click))
+        application.add_handler(CallbackQueryHandler(button_click, pattern='^(valid|invalid)_'))
+        application.add_handler(CallbackQueryHandler(handle_subscription, pattern='^(approve|reject)_'))
         application.run_polling()
     except KeyError:
         print("Error: Environment variable 'PATH_TO_CHANNELS' not found.")
